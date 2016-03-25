e51feff4eee4b82bfbf466d8cf011dfd4cdc8e9b
Sage Weil <sage@redhat.com>
2016-03-25 09:57:45 -0400

    messages/MOSDOp: clear reqid inc for v6 encoding
    For the new v7 encoding, we put the client incarnation in the reqid so that
    we can have it at the beginning of the encoding.  However, we do *not*
    want to do this for the v6 encoding, as that version of get_reqid() will
    return reqid if it is not == osd_reqid_t().  Previously we were setting
    the field in the constructor, but since it's specific to the v7 encoding,
    put it there (in encode_payload()) instead.
    
    Fixes: #15230
    Signed-off-by: Sage Weil <sage@redhat.com>

--- src/messages/MOSDOp.h
+++ src/messages/MOSDOp.h
@@ -168,10 +168,6 @@ public:
       final_decode_needed(false),
       features(feat) {
     set_tid(tid);
-
-    // also put the client_inc in reqid.inc, so that get_reqid() can
-    // be used before the full message is decoded.
-    reqid.inc = inc;
   }
 private:
   ~MOSDOp() {}
@@ -323,7 +319,15 @@ struct ceph_osd_request_head {
       ::encode(osdmap_epoch, payload);
       ::encode(flags, payload);
       ::encode(reassert_version, payload);
-      ::encode(reqid, payload);
+      if (reqid == osd_reqid_t()) {
+	// also put the client_inc in reqid.inc, so that get_reqid() can
+	// be used before the full message is decoded.
+	osd_reqid_t fake_reqid;
+	fake_reqid.inc = client_inc;
+	::encode(fake_reqid, payload);
+      } else {
+	::encode(reqid, payload);
+      }
       ::encode(client_inc, payload);
       ::encode(mtime, payload);
       ::encode(oloc, payload);
